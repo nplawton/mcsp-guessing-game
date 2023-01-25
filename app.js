6fd3ccc3b9b079ff02ffdb97b169b4b06cd35804
nplawton <nplawton02@gmail.com>
2023-01-25 15:49:15 -0800

    completed up to step 5 with attempt at 6

--- app.js
+++ app.js
@@ -8,9 +8,11 @@ let numInt;
 let randomInt;
 let userName;
 let guessCount = 0; // each if statement needs to add 1 to the guessCount to count total guess.
+let guessHisI = []; // guess history stored first time.
+let guessHisF = []; // guess history stored follow on attempts.
 
 //Welcome user to start and establishes the secert number
-function welcome(){
+function init(){
 
     /*
         I: User input of a number; help them get the seceret number within 3 guess or else close loop
@@ -27,59 +29,64 @@ function welcome(){
     
     userName = prompt('what is your name?');
     alert (`Welcome ${userName}!`);
-    return guessing();
 
-}
 
-function guessResults(){
-
+    
     let vaildGuess = false;
+    
 
     while(!vaildGuess){
         const result1 = intialGuess();
         const result2 = followUpGuess();
-        if ((result1 = `${userName} that is correct!`) || (result2 = `${userName} there you go, finally!`)){
+        console.log("Guess history: " + guessHisI + guessHisF);
+        console.log('Times guessed: ' + guessCount);
+
+        if ((result1 === `${userName} that is correct! It took you ${guessCount} guess!`) || (result2 === `${userName} there you go, finally!`)) {
             vaildGuess = true;
+            break;
         }
     }
 
-
+    guessing();
 
 }
 
+
 //allows the user to guess a number
 function guessing(){
+
+    
     //console.log('User is guessing!')
-    numInt = prompt('Pick a number between 1 and 100!');
+    numInt = prompt(`${userName}Pick a number between 1 and 100!`);
     //let num = numInt * 1;
     if (isNaN(numInt)){
         secInt = prompt('Ok wise guy lets try an actual number!');
     }else{
         if (numInt <= 100){
-            alert (`You selected ${numInt}!`);
+            alert (`${userName}, you selected ${numInt}!`);
             
             console.log(numInt);
-            return intialGuess();
+            intialGuess();
         }else{
-            alert ('Shooting for the moon. Try Again!');
-            return location.reload();
+            alert (`${userName}, you'reShooting for the moon. Try Again!`);
+            location.reload();
         }
         
     }
 
     if (isNaN(secInt)){
-        alert ('Go back to school to learn your numbers!');
-        return location.reload();
+        alert (`Go back to school to learn your numbers, ${userName}!`);
+        location.reload();
 
     }else{
         if(secInt <= 100){
             alert (`Now ${secInt} is a number what a smart person!`);
             
             console.log(secInt);
-            return intialGuess();
+            intialGuess();
         }else{
-            alert ('Obviously this is not your day. Try Again!');
-            return location.reload();
+            alert (`Obviously this is not your day, ${userName}. Try Again!`);
+            location.reload();
         }
     }
 }
@@ -90,34 +97,41 @@ function guessing(){
 function intialGuess(){
         
     console.log('Intial guess being judged.');
+    let guessHis1 = [];
+    let guessHis2 = [];
+    guessHisI = guessHis1.concat(guessHis2);
     
     if (!isNaN(numInt)){
+        guessHis1.push(numInt)
         if(numInt > randomInt){
-            alert('A tad to high');
             guessCount = guessCount += 1;
-            return  followUpGuess();
+            alert ( `A tad to high, ${userName}`);
+            followUpGuess();
         }else if (numInt < randomInt){
-            alert('Aim a little higher');
             guessCount = guessCount += 1;
-            return  followUpGuess();
+            alert ('Aim a little higher');
+            followUpGuess();
         }else{
             guessCount = guessCount +=1;
-            alert(`${userName} that is correct! It took you ${guessCount} guess!`);
+            alert (`${userName} that is correct! It took you ${guessCount} guess!`);
+            //replay();
         }
     }
 
     if (!isNaN(secInt)){
+        guessHis2.push(secInt)
         if(secInt > randomInt){
-            alert('A tad to high');
             guessCount = guessCount += 1;
-            return  followUpGuess();
-        }else if (secInt < randomInt){
-            alert('Aim a little higher');
+            alert (`A tad to high, ${userName}`);
+            followUpGuess();
+        }else if (secInt < randomInt[1]){
             guessCount = guessCount += 1;
-            return  followUpGuess();
+            alert (`Aim a little higher, ${userName}`);
+            followUpGuess();
         }else{
             guessCount = guessCount += 1;
-            alert(`${userName} that is correct! It took you ${guessCount} guess!`);
+            alert (`${userName} that is correct! It took you ${guessCount} guess!`);
+            //replay();
         }
     }
 
@@ -132,42 +146,56 @@ function followUpGuess(){
     */
 
     nextInt = prompt('Guess another number between 1 and 100!');
-
+    guessHisF.push(nextInt);
 
     if (!isNaN(nextInt)){
         if (nextInt > randomInt){
-            alert('Aim Lower');
+            alert(`Aim Lower, ${userName}`);
             guessCount = guessCount += 1;
-            return  followUpGuess();
+            followUpGuess();
         }else if (nextInt < randomInt){
-            alert('Tad higher');
             guessCount = guessCount += 1;
-            return  followUpGuess();
+            alert (`Tad higher, ${userName}`);
+            followUpGuess();
         }else {
             guessCount = guessCount += 1;
-            alert(`${userName} there you go,  it only took you ${guessCount} guess!!`);
+            alert (`${userName} there you go,  it only took you ${guessCount} guess!!`);
+            //replay();
         }
     }else{
         alert (`Look, ${userName}, we\'ve already gone over the parametetrs!`);
         guessCount = guessCount += 1;
-        return  followUpGuess();
+        followUpGuess();
     }
 
 }
 
-function leaderBoard(){
-    /*
-       I: Record User attempts and keep top 10 scores
-       O: display the top 10 users attempts
-       C: after 10 drop previous history
-       E: duplicate recorded scores 
-    */
+// function leaderBoard(){
+//     /*
+//        I: Record User attempts and keep top 10 scores && alert user to their score after guessing
+//        O: display the top 10 users attempts
+//        C: after 10 drop previous history
+//        E: duplicate recorded scores 
+//     */
 
-    
+//     let score = 0;
 
-}
+// }
+
+// function replay(){
+
+//     console.log('I\'m here!');
+//     str = '';
+//     let str1 = str.toUpperCase("Y");
+//     let str2 = str.toUpperCase("N");
+//     let replay = prompt(`${userName}, do you want to guess again (Y/N)?`)
+
+//     if(replay === str1){
+//         console.log('Let\'s try this again');
+//         location.reload();
+//     }
+// }
 
 
 //testSite();
-welcome();
-console.log("Guess Count = " + guessCount);
+init();
